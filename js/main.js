de68f40d8cabc85249249d26f3da6a7b91230c83
Jean Paul Gomez <jeanpaul.gomez@gmail.com>
2018-06-19 20:40:26 -0400

    Added accesibility options, changed focus and fixed map position

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", restaurant.photograph_desc);
   li.append(image);
 
   const name = document.createElement('h1');
@@ -155,10 +156,12 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  const more = document.createElement('a');
+  const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.tabIndex=1;
+  more.onclick= ()=>{location.href= DBHelper.urlForRestaurant(restaurant)};
+  //more.href = DBHelper.urlForRestaurant(restaurant);
+  li.append(more);
 
   return li
 }
